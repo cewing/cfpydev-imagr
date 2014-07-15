aadc1e2e972bfa19cdf74924bf60ccdebfa91b23
cewing <cris@crisewing.com>
2014-07-15 16:00:08 -0700

    add basic admin, no customizations

--- imagr_images/admin.py
+++ imagr_images/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from imagr_images.models import (
+    Photo,
+    Album,
+)
+from imagr_users.models import ImagrUser
 
-# Register your models here.
+
+admin.site.register(Photo)
+admin.site.register(Album)
+admin.site.register(ImagrUser)
